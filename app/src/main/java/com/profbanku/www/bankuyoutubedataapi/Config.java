a18a3bc0370141bf4ea4a128fc5a5009a294565a
sudoalgorithm <kunalmalhotra97@gmail.com>
2016-01-27 19:15:42 +0530

    Final Code

---
+++ app/src/main/java/com/profbanku/www/bankuyoutubedataapi/Config.java
@@ -0,0 +1,12 @@
+package com.profbanku.www.bankuyoutubedataapi;
+
+/**
+ * Created by Kunal on 1/22/16.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCu6T4yVAYo9l7lVqXknCATjr9pwfZY1uE";
+}
